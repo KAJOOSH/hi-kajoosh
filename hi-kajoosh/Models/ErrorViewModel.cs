86c871dee8a6bceb240c2e5859940c9c8466132c
emami.s <kajoosh@gmail.com>
2022-10-26 12:53:05 +0330

    Add project files.

---
+++ hi-kajoosh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hi_kajoosh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
